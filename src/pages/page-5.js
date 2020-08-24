facdc393d58e232cedecd19b46d03377620a5938
shuo <caihehuang@gmail.com>
2020-08-25 00:32:31 +0800

    finish page-5

--- src/pages/page-5.js
+++ src/pages/page-5.js
@@ -95,7 +95,6 @@ function HalfCircle() {
         background: yellow,
         borderTopLeftRadius: 200,
         borderTopRightRadius: 200,
-        display: "inline-block",
         originY: 1,
       }}
       animate={{
@@ -150,25 +149,258 @@ function ScaleRight2Left() {
 
 function TwoCircle() {
   const rotate = keyframes`
-  from {left: 0}
-  to {left: 200px}
+  from {left: 0};
+  to {left: 100px};
 `
 
-  const Circle = styled.div`
-    background-color: rebeccapurple;
+  const zindex = keyframes`
+0%, 49.9% {z-index: 1}
+50%,100% {z-index: 0}
+}
+`
+
+  const Circle1 = styled.div`
+    background-color: ${red};
+    border-radius: 100px;
     position: absolute;
     width: 200px;
     height: 200px;
-    animation: ${rotate} 2s linear infinite;
+    animation: ${rotate} 1.5s ease-in-out infinite alternate,
+      ${zindex} 3s infinite reverse;
+  `
+
+  const Circle2 = styled.div`
+    background-color: ${yellow};
+    border-radius: 100px;
+    position: absolute;
+    width: 200px;
+    height: 200px;
+    animation: ${rotate} 1.5s linear infinite alternate-reverse;
   `
   return (
     <div
       style={{
+        width: 300,
+        height: 200,
+      }}
+    >
+      <Circle1 />
+      <Circle2 />
+    </div>
+  )
+}
+
+function Marquee() {
+  let divs = []
+  for (let index = 0; index < 2; index++) {
+    divs.push(
+      <motion.img
+        key={index}
+        src={require("../images/page-5-marquee.svg")}
+        animate={{
+          x: "-100%",
+        }}
+        transition={{
+          duration: 3,
+          ease: "linear",
+          loop: Infinity,
+        }}
+      />
+    )
+  }
+  return (
+    <div
+      style={{
+        width: 200,
+        height: 200,
+        display: "flex",
+        flexDirection: "row",
+        overflow: "hidden",
+      }}
+    >
+      {divs}
+    </div>
+  )
+}
+
+function Spring() {
+  return (
+    <div
+      style={{
+        width: 500,
+        height: 100,
         position: "relative",
       }}
     >
-      <Circle />
+      <motion.div
+        style={{
+          width: 200,
+          height: 100,
+          background: black,
+          originX: 0,
+        }}
+        animate={{
+          scaleX: [1, 1.5, 1],
+        }}
+        transition={{
+          duration: 2,
+          ease: ["easeOut", "easeIn"],
+          flip: Infinity,
+        }}
+      />
+      <motion.div
+        style={{
+          width: 100,
+          height: 100,
+          background: yellow,
+          borderRadius: 50,
+          position: "absolute",
+          top: 0,
+          left: 200,
+        }}
+        animate={{
+          x: [0, 200, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: ["easeOut", "linear"],
+          flip: Infinity,
+        }}
+      />
+      <motion.div
+        style={{
+          width: 100,
+          height: 100,
+          background: red,
+          borderRadius: 50,
+          position: "absolute",
+          top: 0,
+          left: 200,
+        }}
+        animate={{
+          x: [0, 200, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: ["easeOut", "easeIn"],
+          flip: Infinity,
+        }}
+      />
+
+      <motion.div
+        style={{
+          width: 100,
+          height: 100,
+          background: blue,
+          borderRadius: 50,
+          position: "absolute",
+          top: 0,
+          left: 200,
+        }}
+        animate={{
+          x: [0, 200, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: ["easeOut", "circIn"],
+          flip: Infinity,
+        }}
+      />
     </div>
+  )
+}
+
+function Spring2() {
+  return (
+    <div
+      style={{
+        width: 500,
+        height: 100,
+        position: "relative",
+      }}
+    >
+      <motion.div
+        style={{
+          width: 200,
+          height: 100,
+          background: yellow,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          originX: 0,
+        }}
+        animate={{
+          scaleX: [1, 2],
+        }}
+        transition={{
+          duration: 1,
+          ease: ["easeOut"],
+          flip: Infinity,
+        }}
+      />
+      <motion.div
+        style={{
+          width: 200,
+          height: 100,
+          background: blue,
+          originX: 0,
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+        animate={{
+          scaleX: [1, 1.5, 1],
+        }}
+        transition={{
+          duration: 2,
+          ease: ["easeIn", "easeOut"],
+          flip: Infinity,
+        }}
+      />
+      <motion.div
+        style={{
+          width: 100,
+          height: 100,
+          background: red,
+          borderRadius: 50,
+          position: "absolute",
+          top: 0,
+          left: 200,
+        }}
+        animate={{
+          x: [0, 200, 200, 200, 0],
+        }}
+        transition={{
+          duration: 4,
+          ease: ["easeOut", "linear", "linear", "easeOut"],
+          flip: Infinity,
+        }}
+      />
+    </div>
+  )
+}
+
+function Filterball() {
+  return (
+    <motion.div
+      style={{
+        width: 100,
+        height: 100,
+        background: yellow,
+        mixBlendMode: "difference",
+        position: "absolute",
+        borderRadius: 50,
+      }}
+      animate={{
+        x: [100, 300, 300, 100, 100],
+        y: [100, 100, 400, 400, 100],
+      }}
+      transition={{
+        duration: 4,
+        ease: "anticipate",
+        loop: Infinity,
+      }}
+    />
   )
 }
 export default function page5() {
@@ -176,20 +408,35 @@ export default function page5() {
     <Layout>
       <div
         style={{
-          width: 200,
-          height: 200,
-          display: "inline-flex",
+          width: 500,
+          height: 600,
+          display: "flex",
+          flexDirection: "row",
           flexWrap: "wrap",
+          position: "relative",
         }}
       >
-        <Card direction="a" div1Color={red} div2Color={blue} delay={1.5} />
-        <Card direction="d" div1Color={black} div2Color={yellow} delay={0} />
-        <Card direction="b" div1Color={yellow} div2Color={black} delay={0} />
-        <Card direction="c" div1Color={blue} div2Color={red} delay={1.5} />
+        <div
+          style={{
+            width: 200,
+            height: 200,
+            display: "inline-flex",
+            flexWrap: "wrap",
+          }}
+        >
+          <Card direction="a" div1Color={red} div2Color={blue} delay={1.5} />
+          <Card direction="d" div1Color={black} div2Color={yellow} delay={0} />
+          <Card direction="b" div1Color={yellow} div2Color={black} delay={0} />
+          <Card direction="c" div1Color={blue} div2Color={red} delay={1.5} />
+        </div>
+        <HalfCircle />
+        <ScaleRight2Left />
+        <TwoCircle />
+        <Marquee />
+        <Spring />
+        <Spring2 />
+        <Filterball />
       </div>
-      <HalfCircle />
-      <ScaleRight2Left />
-      <TwoCircle />
     </Layout>
   )
 }
