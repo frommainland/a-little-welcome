72f4c6e82a138b04fae1cb906c9b4d017ded152e
shuo <caihehuang@gmail.com>
2020-12-24 09:56:32 +0800

    reverse index link orders, page-11 scrolly init

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,31 @@ const IndexPage = () => (
     <Layout>
         <div className={style.wrap}>
             <ul className={style.list}>
+<<<<<<< HEAD
+                <Link to="/page-11">
+                    <li>
+                        <sup>11.</sup>心驰所向
+                    </li>
+                </Link>
+                <Link to="/page-10">
+                    <li>
+                        <sup>10.</sup>银行标识代码
+                    </li>
+                </Link>
+                <Link to="/page-9">
+                    <li>
+                        <sup>9.</sup>数字支付
+                    </li>
+                </Link>
+                <Link to="/page-8">
+                    <li>
+                        <sup>8.</sup>物联网
+                    </li>
+                </Link>
+                <Link to="/page-7">
+                    <li>
+                        <sup>7.</sup>生物识别
+=======
                 <Link to="/page-1">
                     <li>
                         <sup>1.</sup>跑马灯和网格
@@ -37,6 +62,7 @@ const IndexPage = () => (
                 <Link to="/page-5">
                     <li>
                         <sup>5.</sup>元素
+>>>>>>> 4a0b4f1ba364f52dae76b4197933a7f12d66aefa
                     </li>
                 </Link>
                 <Link to="/page-6">
@@ -44,6 +70,31 @@ const IndexPage = () => (
                         <sup>6.</sup>移动销售终端
                     </li>
                 </Link>
+<<<<<<< HEAD
+                <Link to="/page-5">
+                    <li>
+                        <sup>5.</sup>元素
+                    </li>
+                </Link>
+                <Link to="/page-4">
+                    <li>
+                        <sup>4.</sup>线段排列
+                    </li>
+                </Link>
+                <Link to="/page-3">
+                    <li>
+                        <sup>3.</sup>中心时钟
+                    </li>
+                </Link>
+                <Link to="/page-2">
+                    <li>
+                        <sup>2.</sup>律动竖线
+                    </li>
+                </Link>
+                <Link to="/page-1">
+                    <li>
+                        <sup>1.</sup>跑马灯和网格
+=======
                 <Link to="/page-7">
                     <li>
                         <sup>7.</sup>生物识别
@@ -67,6 +118,7 @@ const IndexPage = () => (
                 <Link to="/page-11">
                     <li>
                         <sup>11.</sup>心驰所向
+>>>>>>> 4a0b4f1ba364f52dae76b4197933a7f12d66aefa
                     </li>
                 </Link>
             </ul>
--- src/pages/page-11.js
+++ src/pages/page-11.js
@@ -1,3 +1,69 @@
+<<<<<<< HEAD
+import React, { useState, useEffect } from "react"
+
+import Layout from "../components/layout"
+import "../components/myfont.css"
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+import useWindowSize from '../components/useWindowSize'
+import S1 from '../components/page11/s1'
+//images links
+import home1 from '../images/page11/home.jpg'
+import arrow from '../images/page11/arrow-down.svg'
+
+
+
+export default function Page11() {
+
+    const size = useWindowSize();
+
+    const { scrollYProgress, scrollY } = useViewportScroll();
+    const [currentY, setCurrentY] = React.useState(0);
+
+    React.useEffect(() => {
+
+        const unsubscribeY = scrollY.onChange((value) =>
+            setCurrentY(Math.round(value))
+        );
+
+        return () => {
+            unsubscribeY();
+        };
+    }, []);
+
+
+    return (
+        <Layout>
+            <div style={{
+                position: 'fixed',
+                left: 10,
+                top: 10,
+                width: 150,
+                height: 30,
+                backgroundColor: 'white',
+                color: 'black'
+            }}>
+                {`window height ${size.height}`}
+            </div>
+
+            <div style={{
+                position: 'fixed',
+                left: 10,
+                top: 50,
+                width: 150,
+                height: 30,
+                backgroundColor: 'white',
+                color: 'black'
+            }}>
+                {`scrollY ${currentY}`}
+            </div>
+
+            <S1 />
+            <div style={{
+                height: '100vh',
+                width: 'auto',
+                background: 'red'
+            }}></div>
+=======
 import React from "react"
 
 import Layout from "../components/layout"
@@ -72,6 +138,7 @@ export default function Page11() {
                     background: 'red'
                 }}></div>
             </div>
+>>>>>>> 4a0b4f1ba364f52dae76b4197933a7f12d66aefa
         </Layout>
     )
 }
